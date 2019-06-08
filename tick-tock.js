ca3dc3e0f9c20dcd021a4756917e8bc4010e724c
Martin Matovu <matovu.synergy@gmail.com>
2019-06-08 11:50:26 +0300

    Defined the getClockTime(), as called by logClockTime(), time which returns clock time as a string

--- tick-tock.js
+++ tick-tock.js
@@ -24,5 +24,37 @@ function logClockTIme(){
     console.log(time);
 }
 
-
+function getClockTime(){
+    // get the current time
+    var date = new Date();
+    var time = "";
+    // serialize clock time
+    var time = {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+        ampm: "AM"
+    }
+    // convert to civilian time
+    if(time.hours ==12){ 
+        time.ampm = "PM"; 
+    } else if(time.hours > 12){ 
+        time.ampm = "PM"; 
+        time.hours -= 12; 
+    }
+    // prepend a '0' on the hours to make double digits
+    if(time.hours < 10){
+        time.hours = "0" + time.hours;
+    }
+    // prepend a '0' on the minutes to make double digits
+    if(time.minutes < 10){
+        time.minutes = "0" + time.minutes;
+    }
+    // prepend a '0' on the seconds to make double digits
+    if(time.seconds < 10){
+        time.seconds = "0" + time.seconds;
+    }
+    // format the clock time as a string "hh:mm:ss tt"
+    return time.hours + ":" + time.minutes + ":" + time.seconds + " " + time.ampm;
+}
  
