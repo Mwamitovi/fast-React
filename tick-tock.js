2645145fea00bf654c014f6cf01c3fabefb6c505
Martin Matovu <matovu.synergy@gmail.com>
2019-06-08 11:40:41 +0300

    Created a "tick-tock" script file, to simulate a ticking clock

---
+++ tick-tock.js
@@ -0,0 +1,13 @@
+
+/**
+ * tick-tock
+ * 
+ * Our challenge is to build a ticking clock. 
+ * The clock should display hours, minutes, seconds and time of day in civilian (normal) time.
+ * Each field must always have double digits,
+ * meaning leading zeros need to be applied to single digit values like 1 or 2. 
+ * The clock must also tick and change the display every second.
+ */
+
+
+ 
