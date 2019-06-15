51a8f885ae7886690c9fde686ae485c4aff96a0b
Martin Matovu <matovu.synergy@gmail.com>
2019-06-15 11:58:35 +0300

    Updated the "process.env.NODE_ENV" == "development" (support faster debugging)

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
 
       new webpack.DefinePlugin({
           "process.env": {
-              NODE_ENV: JSON.stringify("production")
+            //   NODE_ENV: JSON.stringify("production")
+              NODE_ENV: JSON.stringify("development")
           }
       }),
 
