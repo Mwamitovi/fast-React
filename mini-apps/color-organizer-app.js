17a093190d97694517631f84f616dff52e8fc795
Martin Matovu <matovu.synergy@gmail.com>
2019-06-30 15:30:47 +0300

    Create "color-organizer-app.js", and add color (data), and array of objects

---
+++ mini-apps/color-organizer-app.js
@@ -0,0 +1,37 @@
+
+/**
+ * Color Organizer App (summary)
+ * See branch: feature/color-organizer-app (detailed)
+ * 
+ * How to manage state within an app,
+ * that allows users to add, name, rate, and remove colors in their customized lists.
+ * .
+ */
+
+/**
+ * Representational state of our app * 
+ * The entire state of the color organizer can be represented with a single array
+ */
+// The colors, an array of objects
+{
+    colors: [
+        {
+            "id": "0175d1f0-a8c6-41bf-8d02-df5734d829a4",
+            "title": "ocean at dusk",
+            "color": "#00c4e2",
+            "rating": 5
+        },
+        {
+            "id": "83c7ba2f-7392-4d7d-9e23-35adbe186046",
+            "title": "lawn",
+            "color": "#26ac56",
+            "rating": 3
+        },
+        {
+            "id": "a11e3995-b0bd-4d58-8c48-5e49ae7f7f23",
+            "title": "bright red",
+            "color": "#ff0000",
+            "rating": 0
+        }
+    ]
+}
