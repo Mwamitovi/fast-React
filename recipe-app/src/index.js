c40eb6563beaadcce20731b855a4d17c946bbc0d
Martin Matovu <matovu.synergy@gmail.com>
2019-06-30 15:14:11 +0300

    Add comment

--- recipe-app/src/index.js
+++ recipe-app/src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import Menu from './components/Menu'
 import data from '../data/recipes'
 
+// expose the React library globally in the browser
 window.React = React
 
 render(
