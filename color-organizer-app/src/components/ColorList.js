85b1fa99648c0c13da6ec26e7ac1a704f3f8ad97
Martin Matovu <matovu.synergy@gmail.com>
2019-07-07 10:20:24 +0300

    Define "ColorList" component, which maps over a 'colors' array and renders the colors to the DOM

---
+++ color-organizer-app/src/components/ColorList.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import Color from './Color'
+import '../../stylesheets/ColorList.scss'
+
+
+// we map over the array of colors, and display a list of colors
+const ColorList = ({ colors=[], onRate=f=>f, onRemove=f=>f }) => {
+    <div className="color-list">
+        {(colors.length === 0) ?
+            <p>No Colors Listed yet. (Please add a Color)</p> :
+            colors.map(color =>
+                <Color key={color.id}
+                       {...color}
+                       onRate={(rating) => onRate(color.id, rating)}
+                       onRemove={() => onRemove(color.id)} />
+            )
+        }
+    </div>
+}
+
+ColorLust.propTypes = {
+    colors: PropTypes.array,
+    onRate: PropTypes.func,
+    onRemove: PropTypes.func
+}
+
+export default ColorList
