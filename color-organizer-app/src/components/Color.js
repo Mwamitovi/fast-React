5d5bd569ba6d2ac67041b9210bf34edf6ba455c0
Martin Matovu <matovu.synergy@gmail.com>
2019-07-08 13:07:40 +0300

    Debug: <div className="color" style={{ backgroundColor: color}}></div>

--- color-organizer-app/src/components/Color.js
+++ color-organizer-app/src/components/Color.js
@@ -7,7 +7,7 @@ const Color = ({ title, color, rating=0, onRemove=f=>f, onRate=f=>f }) => {
     <section className="color">
         <h1>{title}</h1>
         <button onClick={onRemove}>X</button>
-        <div className="color" styles={{ backgroundColor: color }}></div>
+        <div className="color" style={{ backgroundColor: color }}></div>
         <div>
             <StarRating starsSelected={rating} onRate={onRate} />
         </div>
