fe74fe5c7e03b94b45a23370136c17786a486330
Martin Matovu <matovu.synergy@gmail.com>
2019-07-08 10:07:32 +0300

    Define "App" component: - renders the AddColorForm and ColorList components. - has methods addColor(), rateColor() and removeColor(). - Note that we initiate our color "state" within App component. - And all others are "stateless functional" components

---
+++ color-organizer-app/src/components/App.js
@@ -0,0 +1,65 @@
+import { Component } from 'react'
+import { v4 } from 'uuid'
+import AddColorForm from './AddColorForm'
+import ColorList from './ColorList'
+import '../../stylesheets/App.scss'
+
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            colors: []
+        }
+        this.addColor = this.addColor.bind(this)
+        this.rateColor = this.rateColor.bind(this)
+        this.removeColor = this.removeColor.bind(this)
+    }
+
+    addColor(title, color) {
+        this.setState(prevState => ({
+            colors: [
+                ...prevState.colors,
+                {
+                    id: v4(),
+                    title,
+                    color,
+                    rating: 0
+                }
+            ]
+        }))
+    }
+
+    rateColor(id, rating) {
+        this.setState(prevState => ({
+            colors: prevState.colors.map(_color => 
+                (_color.id !== id) ?
+                    _color :
+                    {
+                        ..._color,
+                        rating
+                    }
+            )
+        }))
+    }
+
+    removeColor(id) {
+        this.setState(prevState => ({
+            colors: prevState.colors.filter(_color => _color.id !== id)
+        }))
+    }
+
+    render() {
+        const { addColor, rateColor, removeColor } = this
+        const { colors } = this.state
+        return (
+            <div classNmae="app">
+                <AddColorForm onNewColor={addColor} />
+                <ColorList colors={colors} 
+                           onRate={rateColor}
+                           onRemove={removeColor} />
+            </div>
+        )
+    }
+}
