b2fed50a6030ea031b955c1f44b8bf19ea277fb0
Martin Matovu <matovu.synergy@gmail.com>
2019-07-25 17:04:20 +0300

    Refactor Color component. - from function to a class, extends Component - Add componentWillMount() method to initialize backgroundColor - Add render() method - and declare Color.defaultProps

--- color-organizer-app/src/components/Color.js
+++ color-organizer-app/src/components/Color.js
@@ -1,25 +1,41 @@
+import { Component } from 'react'
 import PropTypes from 'prop-types'
 import StarRating from './StarRating'
 import '../../stylesheets/Color.scss'
 
 
-const Color = ({ title, color, rating=0, onRemove=f=>f, onRate=f=>f }) => (
-    <section className="color">
-        <h1>{title}</h1>
-        <button onClick={onRemove}>X</button>
-        <div className="color" style={{ backgroundColor: color }}></div>
-        <div>
-            <StarRating starsSelected={rating} onRate={onRate} />
-        </div>
-    </section>
-)
+class Color extends Component {
+
+    componentWillMount() {
+        this.style = { backgroundColor: '#CCC' }
+    }
+
+    render() {
+        const { title, color, rating, onRate } = this.props
+        return (
+            <section className="color" style={this.style}>
+                <h1 ref="title">{title}</h1>
+                <div className="color" style={{ backgroundColor: color }}></div>
+                <div>
+                    <StarRating starsSelected={rating} onRate={onRate} />
+                </div>
+            </section>
+        )
+    }
+}
 
 Color.propTypes = {
     title: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
     rating: PropTypes.number,
-    onRemove: PropTypes.func,
     onRate: PropTypes.func
+}
+
+Color.defaultProps = {
+    title: undefined,
+    rating: 0,
+    color: "#000000",
+    onRate: f=>f
 }
 
 export default Color
