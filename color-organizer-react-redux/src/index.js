48ef2aae31e26a18913fa015f43ca74c699dabbb
Martin Matovu <matovu.synergy@gmail.com>
2019-08-20 14:15:40 +0300

    import a Provider context, and wrap it around App component within render()

--- color-organizer-react-redux/src/index.js
+++ color-organizer-react-redux/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import storeFactory from './store';
 
@@ -17,5 +18,7 @@ window.store = store;
  */
 
 render(
-    <App store={store} />, document.getElementById('react-container')
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('react-container')
 )
