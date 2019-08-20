3ccb25606be3f6a19ac85995c08fe583a00956a1
Martin Matovu <matovu.synergy@gmail.com>
2019-08-20 14:22:04 +0300

    Refactor App, to be a stateless component

--- color-organizer-react-redux/src/components/App.js
+++ color-organizer-react-redux/src/components/App.js
@@ -1,42 +1,12 @@
-import { Component } from 'react';
-import PropTypes from 'prop-types';
-import { sortFunction } from '../lib/array-helpers';
-import '../../stylesheets/App.scss';
+import { Menu, NewColor, Colors } from './containers';
 
 
-class App extends Component {
-
-    getChildContext() {
-        return {
-            store: this.props.store
-        }
-    };
-
-    componentWillMount() {
-        this.unsubscribe = store.subscribe( () => this.forceUpdate() )
-    };
-
-    componentWillUnmount() {
-        this.unsubscribe()
-    };
-
-    render() {
-        return (
-            <div className='app'>
-                <Menu />
-                <NewColor />
-                <Colors />
-            </div>
-        )
-    }
-};
-
-App.propTypes = {
-    store: PropTypes.object.isRequired
-};
-
-App.childContextTypes = {
-    store: PropTypes.object.isRequired
-};
+const App = () => (
+    <div className="app">
+        <Menu />
+        <NewColor />
+        <Colors />
+    </div>
+)
 
 export default App;
