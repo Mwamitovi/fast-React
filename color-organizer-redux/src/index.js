a22bda8655d29aa01565b2abd8fa853f8f47a760
Martin Matovu <matovu.synergy@gmail.com>
2019-08-20 11:02:14 +0300

    Update sortColors() syntax

--- color-organizer-redux/src/index.js
+++ color-organizer-redux/src/index.js
@@ -1,5 +1,5 @@
 import storeFactory from './store'
-import { addColor, removeColor, rateColor, sortColors } from './actions'
+import { addColor, removeColor, rateColor, sortColors } from './actions';
 
 
 const store = storeFactory();
@@ -21,7 +21,7 @@ const store = storeFactory();
 
 store.dispatch(addColor('Uganda Blue', '#1122FF'));
 store.dispatch(rateColor('8658c1d0-9eda-4a90-95e1-8001e8eb6036', 5));
-store.dispatch(sortColor('title'));
+store.dispatch(sortColors('title'));
 
 console.log('current state', store.getState());
 console.log('Go ahead, dispatch some actions...');
