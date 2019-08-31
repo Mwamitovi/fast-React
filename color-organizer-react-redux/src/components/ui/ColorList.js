1427a11161390fe2b4d0f65cda66d2d2cada8d90
Martin Matovu <matovu.synergy@gmail.com>
2019-08-31 11:32:57 +0300

    Update the ColorList, props as sent to the Color component.

--- color-organizer-react-redux/src/components/ui/ColorList.js
+++ color-organizer-react-redux/src/components/ui/ColorList.js
@@ -4,21 +4,26 @@ import '../../stylesheets/ColorList.scss';
 
 
 // we map over the array of colors, and display a list of colors
-const ColorList = ({ colors=[] }) => (
+const ColorList = ({ colors=[], onRate=f=>f, onRemove=f=>f }) => (
     // We just render the Color component
     // All state updates are handled within the Color component directly. 
     <div className="color-list">
         {(colors.length === 0) ?
             <p>No Colors Listed yet. (Please add a Color)</p> :
             colors.map(color =>
-                <Color key={color.id} {...color} />
+                <Color key={color.id} {...color} 
+                    onRate={(rating) => onRate(color.id, rating)}
+                    onRemove={() => onRemove(color.id)}
+                />
             )
         }
     </div>
 );
 
 ColorList.propTypes = {
-    colors: PropTypes.array
+    colors: PropTypes.array,
+    onRate: PropTypes.func,
+    onRemove: PropTypes.func
 };
 
 export default ColorList;
