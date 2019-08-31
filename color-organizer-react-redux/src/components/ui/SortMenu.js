923816c9ec818f74b48a51809180f438520d03b2
Martin Matovu <matovu.synergy@gmail.com>
2019-08-31 11:28:56 +0300

    Update the SortMenu, props to update DOM using the store (via connected)

--- color-organizer-react-redux/src/components/ui/SortMenu.js
+++ color-organizer-react-redux/src/components/ui/SortMenu.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { sortColors } from '../actions';
+import { sortColors } from '../../actions';
 import '../../stylesheets/Menu.scss';
 
 
@@ -10,26 +10,24 @@ const options = {
 };
 
 
-const SortMenu = (props, { store }) => (
+const SortMenu = ({ sort="SORTED_BY_DATE", onSelect=f=>f }) => (
     <nav className="menu">
         <h1>Sort Colors</h1>
         {Object.keys(options).map((item, i) =>
             <a key={i}
                 href="#"
-                className={
-                   (store.getState().sort === options[item]) ? 
-                        "selected" : null
-                }
+                className={(sort === options[item]) ? "selected" : null}
                 onClick={e => {
                     e.preventDefault()
-                    store.dispatch(sortColors(options[item]))
+                    onSelect(options[item])
                 }}>{item}</a>
         )}
     </nav>
 );
 
-SortMenu.contextTypes = {
-    store: PropTypes.object
+SortMenu.propTypes = {
+    sort: PropTypes.string,
+    onSelect: PropTypes.func
 };
 
 export default SortMenu;
