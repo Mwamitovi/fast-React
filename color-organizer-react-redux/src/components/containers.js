c7386f126e136f51732eedc1261a83a268a89956
Martin Matovu <matovu.synergy@gmail.com>
2019-08-31 11:22:15 +0300

    Utilize connect() to create components (Menu/NewColor/Colors) that retrieve the store

--- color-organizer-react-redux/src/components/containers.js
+++ color-organizer-react-redux/src/components/containers.js
@@ -1,3 +1,4 @@
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AddColorForm from './ui/AddColorForm';
 import SortMenu from './ui/SortMenu';
@@ -6,43 +7,38 @@ import { addColor, removeColor, rateColor, sortColors } from '../actions';
 import { sortFunction } from '../lib/array-helpers';
 
 
-export const Menu = (props, {store}) => (
-    <SortMenu 
-        sort={store.getState().sort}
-        onSelect={sortBy => store.dispatch(sortColors(sortBy))}
-    />
-);
-
-Menu.contextTypes = {
-    store: PropTypes.object
-};
-
-
-export const NewColor = (props, {store}) => (
-    <AddColorForm 
-        onNewColor={(title, color) => store.dispatch( addColor(title, color))}
-    />
-);
-
-NewColor.contextTypes = {
-    store: PropTypes.object
-};
+export const Menu = connect(
+    state => ({
+        sort: state.sort
+    }),
+    dispatch => ({
+        onSelect(sortBy) {
+            dispatch(sortColors(sortBy))
+        }
+    })
+)(SortMenu)
 
 
-export const Colors = (props, {store}) => {
+export const NewColor = connect(
+    null,
+    dispatch => ({
+        onNewColor(title, color) {
+            dispatch(addColor(title, color))
+        }
+    })
+)(AddColorForm)
 
-    const { colors, sort } = store.getState();
-    const sortedColors = [...colors].sort(sortFunction(sort));
 
-    return (
-        <ColorList 
-            colors={sortedColors}
-            onRemove={(id) => store.dispatch( removeColor(id) )}
-            onRate={(id, rating) => store.dispatch( rateColor(id, rating))}
-        />
-    )
-};
-
-Colors.contextTypes = {
-    store: PropTypes.object
-};
+export const Colors = connect(
+    state => ({
+        colors: [...state.colors].sort(sortFunction(state.sort))
+    }),
+    dispatch => ({
+        onRemove(id) {
+            dispatch(removeColor(id))
+        },
+        onRate(id, rating) {
+            dispatch(rateColor(id, rating))
+        }
+    })
+)(ColorList)
