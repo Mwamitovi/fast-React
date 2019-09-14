b24644ad4816e7627ddc9c40769847f7d69f0093
Martin Matovu <matovu.synergy@gmail.com>
2019-09-14 14:03:12 +0300

    Update webpack config

--- site.webpack.config.js
+++ site.webpack.config.js
@@ -7,10 +7,10 @@ process.noDeprecation = true
 module.exports = {
   entry: "./company-website/src/index.js",
   output: {
-      path: path.join(__dirname, 'company-website/dist', 'assets'),
-      filename: "bundle.js",
-      publicPath: "assets",
-      sourceMapFilename: 'bundle.map'
+    path: path.join(__dirname, 'company-website/dist', 'assets'),
+    filename: "bundle.js",
+    publicPath: "assets",
+    sourceMapFilename: 'bundle.map'
   },
   devtool: '#source-map',
   devServer: {
@@ -19,50 +19,52 @@ module.exports = {
     port: 3000
   },
   module: {
-      rules: [
-          {
-              test: /\.js$/,
-              exclude: /(node_modules)/,
-              loader: 'babel-loader',
-              query: {
-                  presets: ['env', 'stage-0', 'react']
-              }
-          },
-          {
-            test: /\.css$/,
-            use: ['style-loader','css-loader', {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: () => [require('autoprefixer')]
-                }}]
-          },
-          {
-            test: /\.scss/,
-            use: ['style-loader','css-loader', {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: () => [require('autoprefixer')]
-                }}, 'sass-loader']
-          }        
-      ]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'stage-0', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => [require('autoprefixer')]
+          }
+        }]
+      },
+      {
+        test: /\.scss/,
+        use: ['style-loader', 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => [require('autoprefixer')]
+          }
+        }, 'sass-loader']
+      }
+    ]
   },
   plugins: [
-      new webpack.DefinePlugin({
-          "process.env": {
-            //   NODE_ENV: JSON.stringify("production")
-              NODE_ENV: JSON.stringify("development")
-          }
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-          sourceMap: true,
-          warnings: false,
-          mangle: false
-      }),
-      new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.optimize\.css$/g,
-        cssProcessor: require('cssnano'),
-        cssProcessorOptions: {discardComments: {removeAll: true}},
-        canPrint: true
+    new webpack.DefinePlugin({
+      "process.env": {
+        //   NODE_ENV: JSON.stringify("production")
+        NODE_ENV: JSON.stringify("development")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      warnings: false,
+      mangle: false
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
     })
   ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,70 @@
 var webpack = require("webpack");
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 var path = require("path");
 
 process.noDeprecation = true
 
 module.exports = {
-  entry: "./color-organizer-redux/src/index.js",
+  entry: "./color-organizer-router/src/index.js",
   output: {
-      path: path.join(__dirname, 'color-organizer-redux/dist', 'assets'),
-      filename: "bundle.js",
-      sourceMapFilename: 'bundle.map'
+    path: path.join(__dirname, 'color-organizer-router/dist', 'assets'),
+    filename: "bundle.js",
+    publicPath: "assets",
+    sourceMapFilename: 'bundle.map'
   },
   devtool: '#source-map',
+  devServer: {
+    inline: true,
+    contentBase: './color-organizer-router/dist',
+    port: 3000
+  },
   module: {
-      rules: [
-          {
-              test: /\.js$/,
-              exclude: /(node_modules)/,
-              loader: 'babel-loader',
-              query: {
-                  presets: ['env', 'stage-0', 'react']
-              }
-          },
-          {
-            test: /\.css$/,
-            use: ['style-loader','css-loader', {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: () => [require('autoprefixer')]
-                }}]
-          },
-          {
-            test: /\.scss/,
-            use: ['style-loader','css-loader', {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: () => [require('autoprefixer')]
-                }}, 'sass-loader']
-          }                  
-      ]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'stage-0', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => [require('autoprefixer')]
+          }
+        }]
+      },
+      {
+        test: /\.scss/,
+        use: ['style-loader', 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => [require('autoprefixer')]
+          }
+        }, 'sass-loader']
+      }
+    ]
   },
   plugins: [
-      new webpack.DefinePlugin({
-          "process.env": {
-            //   NODE_ENV: JSON.stringify("production")
-              NODE_ENV: JSON.stringify("development")
-          }
-      }),
-
-      new webpack.optimize.UglifyJsPlugin({
-          sourceMap: true,
-          warnings: false,
-          mangle: false
-      })
+    new webpack.DefinePlugin({
+      "process.env": {
+        //   NODE_ENV: JSON.stringify("production")
+        NODE_ENV: JSON.stringify("development")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      warnings: false,
+      mangle: false
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    })
   ]
 }
