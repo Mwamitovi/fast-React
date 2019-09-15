d76be725fd8dcf46f6950147a16f9e0b74502df8
Martin Matovu <matovu.synergy@gmail.com>
2019-09-15 15:25:34 +0300

    Update to render htmlResponse

--- color-organizer/src/server/app.js
+++ color-organizer/src/server/app.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import path from 'path'
 import fs from 'fs'
+import { Provider } from 'react-redux'
+import { compose } from 'redux'
+import { renderToString } from 'react-dom/server'
+import { StaticRouter } from 'react-router-dom'
+import App from '../components/App'
 import storeFactory from '../store'
 import initialState from '../../data/initialState.json'
+
+const staticCSS = fs.readFileSync(
+  path.join(__dirname, '../../dist/assets/bundle.css')
+)
 
 const fileAssets = express.static(
   path.join(__dirname, '../../dist/assets')
@@ -13,33 +22,69 @@ const serverStore = storeFactory(true, initialState)
 serverStore.subscribe(
   () => fs.writeFile(
     path.join(__dirname, '../../data/initialState.json'),
-    JSON.stringify(serverStore.getState()), 
+    JSON.stringify(serverStore.getState()),
     error => (error) ?
       console.log("Error saving state!", error) :
       null
-    )
   )
+)
+
+const buildHTMLPage = ({ html, state, css }) => (`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta name="viewport" content="minimum-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no" />
+      <meta charset="utf-8">
+       <title>Universal Color Organizer</title>
+      <style>${staticCSS}</style>
+    </head>
+    <body>
+      <div id="react-container">${html}</div>
+      <script>window.__INITIAL_STATE__ = ${JSON.stringify(state)}</script>
+      <script src="/bundle.js"></script>
+    </body>
+  </html>
+`)
+
+const renderComponentsToHTML = ({ url, store }) =>
+  ({
+    state: store.getState(),
+    html: renderToString(
+      <Provider store={store}>
+        <StaticRouter location={url} context={{}}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    )
+  })
+
+const makeClientStoreFrom = store => url =>
+  ({
+    url,
+    store: storeFactory(false, store.getState())
+  })
+
+const htmlResponse = compose(
+  buildHTMLPage,
+  renderComponentsToHTML,
+  makeClientStoreFrom(serverStore)
+)
+
+const respond = (req, res) =>
+  res.status(200).send(htmlResponse(req.url))
 
 const logger = (req, res, next) => {
   console.log(`${req.method} request for '${req.url}'`)
   next()
 }
 
-const respond = (req, res) =>
-  res.status(200).send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Universal Color Organizer</title>
-      </head>
-      <body>
-        <div id="react-container">ready...</div>
-      </body>
-    </html>
-  `)
+const addStoreToRequestPipeline = (req, res, next) => {
+  req.store = serverStore
+  next()
+}
 
 export default express()
   .use(logger)
   .use(fileAssets)
+  .use(addStoreToRequestPipeline)
   .use(respond)
