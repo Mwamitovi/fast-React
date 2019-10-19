a36dd4c006901a70caa5dbf58633fadd58f36e9c
Martin Matovu <matovu.synergy@gmail.com>
2019-10-19 21:10:31 +0300

    TDD: Write minimal test for `color` reducer

---
+++ color-organizer-test/__tests__/__store__/__reducers__/color.test.js
@@ -0,0 +1,51 @@
+import C from '../../../src/constants'
+import { color } from '../../../src/store/reducers'
+
+/* eslint-disable no-undef */
+
+describe('color Reducer', () => {
+
+  it('ADD_COLOR success', () => {
+    const state = {}
+    const action = {
+      type: C.ADD_COLOR,
+      id: 0,
+      title: 'Test Teal',
+      color: '#90C3D4',
+      timestamp: new Date().toString()
+    }
+    const results = color(state, action)
+    expect(results)
+      .toEqual({
+        id: 0,
+        title: 'Test Teal',
+        color: '#90C3D4',
+        timestamp: action.timestamp,
+        rating: 0
+      })
+  })
+
+  it('RATE_COLOR success', () => {
+    const state = {
+      id: 0,
+      title: 'Test Teal',
+      color: '#90C3D4',
+      timestamp: 'Sat Oct 19 2019 13:32:09 GMT+0300 (EAT)',
+      rating: undefined
+    }
+    const action = {
+      type: C.RATE_COLOR,
+      id: 0,
+      rating: 3
+    }
+    const results = color(state, action)
+    expect(results)
+      .toEqual({
+        id: 0,
+        title: 'Test Teal',
+        color: '#90C3D4',
+        timestamp: 'Sat Oct 19 2019 13:32:09 GMT+0300 (EAT)',
+        rating: 3
+      })
+  })
+})
